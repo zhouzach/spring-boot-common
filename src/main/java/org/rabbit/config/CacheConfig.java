d2f91e0b04cce486337737deb96855abf28b4056
Zach Zhou <769087026@qq.com>
2019-10-26 16:23:12 +0800

    add cacheManager doc

--- src/main/java/org/rabbit/config/CacheConfig.java
+++ src/main/java/org/rabbit/config/CacheConfig.java
@@ -2,7 +2,9 @@
 //
 //import org.springframework.cache.annotation.EnableCaching;
 //import org.springframework.context.annotation.Configuration;
-//
+// 集成多CacheManager
+//https://www.twblogs.net/a/5c81f63fbd9eee35fc138743
+//https://segmentfault.com/a/1190000013269653
 //@Configuration
 //@EnableCaching
 //public class CacheConfig {

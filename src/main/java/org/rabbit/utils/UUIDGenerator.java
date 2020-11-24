8eeb4d30a2bf9ca50bbd2a20eca1c7bd97010cf7
Zhou Zach <wander669@gmail.com>
2020-11-24 16:33:23 +0800

    add UUIDGenerator

--- src/main/java/org/rabbit/utils/UUIDGenerator.java
+++ src/main/java/org/rabbit/utils/UUIDGenerator.java
@@ -9,4 +9,8 @@ public class UUIDGenerator {
     public static UUID generate(){
         return Generators.timeBasedGenerator().generate();
     }
+
+    public static void main(String[] args){
+        System.out.println(generate());
+    }
 }
